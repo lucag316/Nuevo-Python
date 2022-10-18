05a0895294500f7c5d9e18e418271184d9876437
lucag316 <lucafranco316@gmail.com>
2022-10-18 00:09:06 -0300

    Update

--- Clase_5/Desafio_02.py
+++ Clase_5/Desafio_02.py
@@ -43,7 +43,6 @@ def stark_normalizar_datos(lista_personajes:list, clave:str):
         print("ERROR, lista de heroes vacia")
 #----------------------------------------------0-------------------------------------------------------------------------
 
-
 def obtener_nombre(personaje:dict)->str:
     '''
     Recibe por parametro a un personaje
@@ -87,7 +86,6 @@ def obtener_nombre_y_dato(personaje:dict, key:str):
 
     return mensaje
 #----------------------------------------------2-----------------------------------------------------------------------
-#obtener_nombre_y_dato(lista_personajes[0], "altura")
 
 def stark_imprimir_nombres_alturas(lista_personajes:list):
     '''
@@ -105,7 +103,7 @@ def stark_imprimir_nombres_alturas(lista_personajes:list):
 
     return retono
 #----------------------------------------------3-----------------------------------------------------------------------
-#stark_imprimir_nombres_alturas(lista_personajes)
+
 def calcular_max(lista_personajes:list, key:str):
     '''
     Calcula el maximo de la lista  segun la clave elegida
@@ -154,33 +152,195 @@ def calcular_max_min_dato(lista_personajes:list, clave:str, tipo:str):
     return retorno
 #----------------------------------------------4.3----------------------------------------------------------------------
 
-def stark_calcular_imprimir_heroe():
+def stark_calcular_imprimir_heroe(lista_personajes:list, key:str, tipo:str):
     '''
+    Reutiliza las funciones 1.2, 2 y 4.3, calcula al personaje segun lo pedido y muestra su nombre y la clave calculada
+
+    Recibe por parametros, la lista de personajes, la clave del dato a calcular, y el tipo de calculo(maximo o minimo)
+
+    obtiene al personaje que cumple con las condiciones, Imprime el nombre del personaje y el valor calculado
     '''
+    if(len(lista_personajes) > 0):
+        datos_personaje = calcular_max_min_dato(lista_personajes, key, tipo)
+        datos_personaje = obtener_nombre_y_dato(datos_personaje, key)
+        
+    else:
+        datos_personaje = -1
+
+    imprimir_dato(datos_personaje)
 #----------------------------------------------4.4----------------------------------------------------------------------
 
-#4.4- Crear la función 'stark_calcular_imprimir_heroe' la cual recibirá tres parámetros: 
-# La lista de héroes
-# El tipo de cálculo a realizar: es un valor string que puede tomar los valores ‘maximo’ o ‘minimo’
-# Un string que representa la key del dato a calcular, por ejemplo: ‘altura’, ‘peso’, ‘edad’, etc.
-# Con este se resuelve el Ej 3, Ej 4, Ej 6 y Ej 7 del desafío #00
-# La función deberá obtener el héroe que cumpla dichas condiciones, imprimir su nombre y el valor calculado. Reutilizar las funciones de los puntos:
-# punto 1.2, punto: 2 y punto 4.3 
-# Validar que la lista de héroes no esté vacía para realizar sus acciones, caso contrario no hará nada y retornara -1.
-# Ejemplo de llamada:
-#  	stark_calcular_imprimir_heroe (lista, "maximo", "edad")
-#             Ejemplo de salida:
-# 	Mayor altura: Nombre: Howard the Duck | altura: 79.34
+def sumar_dato_heroe(lista_personajes:list, key:str):
+    '''
+    Suma  segun la clave que desea 
+
+    Recibe la lista de personajes y una clave
+
+    Retorna la suma completa de todo los personajes segun la clave que ingrese
+    '''
+    suma = 0
+
+    for personaje in lista_personajes:
+        if(type(personaje) != type(dict()) or personaje == ""):
+            suma = -1
+        else:
+            suma += float(personaje[key])
+
+    return suma
+#----------------------------------------------5.1----------------------------------------------------------------------
+
+def dividir(dividendo:int, divisor:int) -> float:
+    '''
+    Realiza la division entre los dos parametros
+
+    Recibe 2 parametros tipo int(dividendo y divisor)
+
+    Retorna la division hecha, en caso de que el divisor sea 0 retorna un 0
+    '''
+    if(divisor == 0):
+        division = 0
+    else: 
+        division = dividendo / divisor
+    
+    return division
+#----------------------------------------------5.2----------------------------------------------------------------------
+def calcular_promedio(lista_personajes:list, key:str):
+    '''
+    Calcula el promedio segun el dato pasado, reutiliza las funciones 5.1 y 5.2
+
+    Recibe la lista de personajes y la clave que se requiere calcular el promedio
+
+    Retorna el promedio del dato pasado por parametro
+    '''
+    cantidad_personajes = len(lista_personajes) 
+    suma = sumar_dato_heroe(lista_personajes,  key)
+    
+    promedio = dividir(suma, cantidad_personajes)
+
+    return promedio
+#----------------------------------------------5.3----------------------------------------------------------------------
+def stark_calcular_imprimir_promedio_altura(lista_personajes:list):
+    '''
+    Calcula la altura promedio, reutiliza las funciones 1.2 y 5.3
+
+    Recibe por parametro la lista de personajes
+
+    Imprime la altura promedio
+    '''
+    if(lista_personajes == ""):
+        promedio_altura = -1
+    else:
+        promedio_altura = calcular_promedio(lista_personajes, "altura")
+
+    imprimir_dato(promedio_altura)
+#----------------------------------------------5.4----------------------------------------------------------------------
+
+def imprimir_menu():
+    '''
+    Imprime el menu de opciones, reutiliza la funcion 1.2
+    '''
+
+    while True:
+
+        print("1- Imprimir nombres de los personajes \n2- Imprimir nombre y alturas \n3- Calcular heroe segun su clave y tipo de calculo \n4- Calcular promedio segun la  clave \n5- Promedio altura   \n6- Salir")
+
+        respuesta = input()
+
+        if(respuesta == "1"):
+            stark_imprimir_nombres_heroes(lista_personajes)
+        elif(respuesta == "2"):
+            stark_imprimir_nombres_alturas(lista_personajes)
+        elif(respuesta == "3"):
+
+            key = input("Ingrese la clave que desea calcular (altura, peso,  fuerza):  ")
+            while(key  != "altura" and key != "peso" and key != "fuerza"):
+                key = input("ERROR, reingrese la clave que desea calcular (altura, peso,  fuerza):  ")
+
+            tipo = input("Ingrese el tipo de calculo(maximo o minimo):  ")
+            while(tipo != "maximo" and  tipo != "minimo"):
+                tipo = input("ERROR, reingrese el tipo de calculo(maximo o minimo):  ")
+
+            stark_calcular_imprimir_heroe(lista_personajes, key, tipo)
+        elif(respuesta == "4"):
+
+            key = input("Ingrese la clave que desea calcular (altura, peso,  fuerza):  ")
+            while(key  != "altura" and key != "peso" and key != "fuerza"):
+                key = input("ERROR, reingrese la clave que desea calcular (altura, peso,  fuerza):  ")
+
+            calcular_promedio(lista_personajes, key)
+        elif(respuesta == "5"):
+            stark_calcular_imprimir_promedio_altura(lista_personajes)
+        elif(respuesta == "6"):
+            break
+#----------------------------------------------6.1----------------------------------------------------------------------
+imprimir_menu()
+# 6.1-  Crear la función "imprimir_menu" que imprima el menú de opciones por pantalla, el cual permite utilizar toda la funcionalidad ya programada. Se deberá reutilizar la función antes creada encargada de imprimir un string (1.2)
 
 
 
-def menu():
-    print()
 
-    respuesta = input()
 
-    if(respuesta == "1"):
+
+
+
+def validar_entero(numero:str):
+    '''
+    Recibe como parametro un numero de tipo string, el cual se verifica que este conformado solo por digitos
+
+    Retorna True en caso de serlo, False caso contrario
+    '''
+    if(type(numero) == type(str())):
         pass
-    elif(respuesta == "2"):
-        pass
+#----------------------------------------------6.2----------------------------------------------------------------------
 
+# 6.2- Crear la función “validar_entero” la cual recibirá por parámetro un string de número el cual deberá verificar que sea un string conformado únicamente por dígitos. Retornara True en caso de serlo, False caso contrario
+
+
+
+
+
+
+
+
+def stark_menu_principal():
+    '''
+    Imprime el  menu de opciones
+    '''
+#----------------------------------------------6.3----------------------------------------------------------------------
+
+# 6.3- Crear la función 'stark_menu_principal' la cual se encargará de imprimir el menú de opciones y le pedirá al usuario que ingrese el número de una de las opciones elegidas y deberá validarlo. En caso de ser correcto dicho número, lo retornara casteado a entero, caso contrario retorna -1. Reutilizar las funciones del ejercicio 6.1 y 6.2
+
+
+
+
+def stark_marvel_app():
+    '''
+    '''
+#----------------------------------------------7----------------------------------------------------------------------
+
+# 7- Crear la función 'stark_marvel_app' la cual recibirá por parámetro la lista de héroes y se encargará de la ejecución principal de nuestro programa. 
+# Utilizar if/elif o match según prefiera (match solo para los que cuentan con python 3.10+). Debe informar por consola en caso de seleccionar una opción incorrecta y volver a pedir el dato al usuario. Reutilizar las funciones con prefijo 'stark_' donde crea correspondiente.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#EN EL 5.4 NO SE COMO USAR LA  FUNCION 5.1 (ESTA HECHO PERO SIN REUTILIZAR LA OTRA FUNCION)
--- Clase_5/notas.py
+++ Clase_5/notas.py
@@ -17,12 +17,12 @@
 
 
 
-#1.3-  Crear la función 'stark_imprimir_nombres_heroes' la cual recibirá por parámetro la lista de héroes y deberá imprimirla en la consola. Reutilizar las funciones hechas en los puntos 1.1 y 1.2. Validar que la lista no esté vacía para realizar sus acciones, caso contrario no hará nada y retornara -1.
+# 1.3-  Crear la función 'stark_imprimir_nombres_heroes' la cual recibirá por parámetro la lista de héroes y deberá imprimirla en la consola. Reutilizar las funciones hechas en los puntos 1.1 y 1.2. Validar que la lista no esté vacía para realizar sus acciones, caso contrario no hará nada y retornara -1.
 # Con este se resuelve el Ej 1 del desafío #00
 
 
 
-#2-  Crear la función 'obtener_nombre_y_dato' la misma recibirá por parámetro un diccionario el cual representara a un héroe y una key (string) la cual representará el dato que se desea obtener. 
+# 2-  Crear la función 'obtener_nombre_y_dato' la misma recibirá por parámetro un diccionario el cual representara a un héroe y una key (string) la cual representará el dato que se desea obtener. 
 
 # La función deberá devolver un string el cual contenga el nombre y dato (key) del héroe a imprimir. El dato puede ser 'altura', 'fuerza', 'peso' O CUALQUIER OTRO DATO.
 
@@ -36,14 +36,14 @@
 
 
 
-#4.1-  Crear la función 'calcular_max' la cual recibirá por parámetro la lista de héroes y una key (string) la cual representará el dato que deberá ser evaluado a efectos de determinar cuál es el máximo de la lista. Por ejemplo la función deberá poder calcular: el peso, la altura o fuerza máximas y retornar el héroe que tenga el dato más alto.
+# 4.1-  Crear la función 'calcular_max' la cual recibirá por parámetro la lista de héroes y una key (string) la cual representará el dato que deberá ser evaluado a efectos de determinar cuál es el máximo de la lista. Por ejemplo la función deberá poder calcular: el peso, la altura o fuerza máximas y retornar el héroe que tenga el dato más alto.
 # Ejemplo de llamada:
 # 	calcular_max(lista, 'edad')
 
 
 
 
-#4.2-  Crear la función 'calcular_min' la cual recibirá por parámetro la lista de héroes y una key (string) la cual representará el dato que deberá ser evaluado a efectos de determinar cuál es el mínimo de la lista. Por ejemplo la función deberá poder calcular: el peso, la altura o fuerza máximas y retornar el héroe que tenga el dato más bajo. 
+# 4.2-  Crear la función 'calcular_min' la cual recibirá por parámetro la lista de héroes y una key (string) la cual representará el dato que deberá ser evaluado a efectos de determinar cuál es el mínimo de la lista. Por ejemplo la función deberá poder calcular: el peso, la altura o fuerza máximas y retornar el héroe que tenga el dato más bajo. 
 # Ejemplo de llamada:
 # 	calcular_min(lista, 'edad')
 
@@ -51,7 +51,7 @@
 
 
 
-#4.3- Crear la funcion 'calcular_max_min_dato' la cual recibira tres parámetros:
+# 4.3- Crear la funcion 'calcular_max_min_dato' la cual recibira tres parámetros:
 # La lista de héroes
 # El tipo de cálculo a realizar: es un valor string que puede tomar los valores ‘maximo’ o ‘minimo’
 # Un string que representa la key del dato a calcular, por ejemplo: ‘altura’, ‘peso’, ‘edad’, etc.
@@ -64,7 +64,7 @@
 
 
 
-#4.4- Crear la función 'stark_calcular_imprimir_heroe' la cual recibirá tres parámetros: 
+# 4.4- Crear la función 'stark_calcular_imprimir_heroe' la cual recibirá tres parámetros: 
 # La lista de héroes
 # El tipo de cálculo a realizar: es un valor string que puede tomar los valores ‘maximo’ o ‘minimo’
 # Un string que representa la key del dato a calcular, por ejemplo: ‘altura’, ‘peso’, ‘edad’, etc.
@@ -76,3 +76,53 @@
 #  	stark_calcular_imprimir_heroe (lista, "maximo", "edad")
 #             Ejemplo de salida:
 # 	Mayor altura: Nombre: Howard the Duck | altura: 79.34
+
+
+
+
+
+# 5.1- Crear la función 'sumar_dato_heroe' la cual recibirá como parámetro una lista de héroes y un string que representara el dato/key de los héroes que se requiere sumar. Validar que cada héroe sea tipo diccionario y que no sea un diccionario vacío antes de hacer la suma. La función deberá retorna la suma de todos los datos según la key pasada por parámetro
+
+
+
+
+# 5.2- Crear la función  ‘dividir’ la cual recibirá como parámetro dos números (dividendo y divisor). Se debe verificar si el divisor es 0,  en caso de serlo, retornar 0, caso contrario realizar la división entre los parámetros y retornar el resultado
+
+
+
+
+# 5.3- Crear la función ‘calcular_promedio’ la cual recibirá como parámetro una lista de héroes y un string que representa el dato del héroe que se requiere calcular el promedio. La función debe retornar el promedio del dato pasado por parámetro
+# IMPORTANTE: hacer uso de las las funciones creadas en los puntos 5.1 y 5.2
+
+
+
+
+
+# 5.4- Crear la función 'stark_calcular_imprimir_promedio_altura' la cual recibirá una lista de héroes y utilizando la función del punto 5.3 calcula y mostrará la altura promedio. Validar que la lista de héroes no esté vacía para realizar sus acciones, caso contrario no hará nada y retornara -1.
+# IMPORTANTE: hacer uso de las las funciones creadas en los puntos 1.2, 5.1 y 5.3
+# Con este se resuelve el Ej 5 del desafío #00
+
+
+
+
+
+# 6.1-  Crear la función "imprimir_menu" que imprima el menú de opciones por pantalla, el cual permite utilizar toda la funcionalidad ya programada. Se deberá reutilizar la función antes creada encargada de imprimir un string (1.2)
+
+
+
+
+
+# 6.2- Crear la función “validar_entero” la cual recibirá por parámetro un string de número el cual deberá verificar que sea un string conformado únicamente por dígitos. Retornara True en caso de serlo, False caso contrario
+
+
+
+
+# 6.3- Crear la función 'stark_menu_principal' la cual se encargará de imprimir el menú de opciones y le pedirá al usuario que ingrese el número de una de las opciones elegidas y deberá validarlo. En caso de ser correcto dicho número, lo retornara casteado a entero, caso contrario retorna -1. Reutilizar las funciones del ejercicio 6.1 y 6.2
+
+
+
+
+
+
+# 7- Crear la función 'stark_marvel_app' la cual recibirá por parámetro la lista de héroes y se encargará de la ejecución principal de nuestro programa. 
+# Utilizar if/elif o match según prefiera (match solo para los que cuentan con python 3.10+). Debe informar por consola en caso de seleccionar una opción incorrecta y volver a pedir el dato al usuario. Reutilizar las funciones con prefijo 'stark_' donde crea correspondiente.
