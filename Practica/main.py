73fa293eb22b1c86d18b8afb3c153f496a8b204d
lucag316 <lucafranco316@gmail.com>
2022-10-26 17:10:56 -0300

    Update

--- Practica/main.py
+++ Practica/main.py
@@ -12,7 +12,13 @@ pygame.display.set_caption("Luca Game")
 running = True
 
 posicion_circulo = [200, 600]
+posicion_circulo_2 = [100, 600]
 
+coordenada_x = 20
+coordenada_y = 550
+
+speed_x = 0
+speed_y = 0
 
 # TIMER
 timer_segundo = pygame.USEREVENT
@@ -31,7 +37,7 @@ while running:
             print(event.pos)
         if event.type == pygame.USEREVENT:
             if event.type == timer_segundo:
-                print("ya paso un segundo")
+                #print("ya paso un segundo")
                 # if posicion_circulo[0] > 400:
                 #     posicion_circulo[0] *= -1
                 #     if posicion_circulo[0] < 200:
@@ -42,30 +48,46 @@ while running:
                 else:
                     posicion_circulo[0] = -10
                     # ESTE IF ELSE LO PUEDO  USAR POR EJEMPLO SI QUIERO QUE EL FONDO SE VEA MOVIENDOSE, EJEMPLO UN AUTO EN LA RUTA AVANZANDO
-        if event.type == pygame.KEYDOWN:
-            if event.type == pygame.K_LEFT:
-                pass
-            if event.type == pygame.K_RIGHT:
-                pass
-            if event.type == pygame.K_UP:
-                pass
-            if event.type == pygame.K_DOWN:
-                pass
+        
+
+
+            #EVENTOS TECLADO 
+        if(event.type == pygame.KEYDOWN):
+
+
+            if(event.key == pygame.K_LEFT):
+                speed_x = -1
+            if(event.key == pygame.K_RIGHT):
+                speed_x = 1
+            if(event.key == pygame.K_UP):
+                speed_y = -1
+            if(event.key == pygame.K_DOWN):
+                speed_y = 1
+
+        if(event.type == pygame.KEYUP):
+
+            if(event.key == pygame.K_LEFT):
+                speed_x = 0
+            if(event.key == pygame.K_RIGHT):
+                speed_x = 0
+            if(event.key == pygame.K_UP):
+                speed_y = 0
+            if(event.key == pygame.K_DOWN):
+                speed_y = 0
             
-
-
-
-
-
 
 
 
     screen.fill(colores.WHITE)
 
+    coordenada_x += speed_x
+    coordenada_y += speed_y
+
     #---------------ZONA DE DIBUJO------------------
     pygame.draw.rect(screen, colores.ORANGE, (0, 600, 1200, 700)) # donde empieza x, y, donde terminan x, y
     pygame.draw.circle(screen, colores.BROWN, posicion_circulo, 10)
-    pygame.draw. rect(screen, colores.BLACK, (20, 550, 50, 50))
+    pygame.draw.circle(screen, colores.PINK, posicion_circulo_2, 30)
+    pygame.draw. rect(screen, colores.BLACK, [coordenada_x, coordenada_y, 50, 50])
 
     #---------------ZONA DE DIBUJO------------------      
       
