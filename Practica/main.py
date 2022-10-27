bb30b7e5cf0751e3507eba4769ee72d5d5feadf4
lucag316 <lucafranco316@gmail.com>
2022-10-27 16:55:08 -0300

    Update

--- Practica/main.py
+++ Practica/main.py
@@ -10,6 +10,8 @@ screen = pygame.display.set_mode([ANCHO_VENTANA, ALTO_VENTANA])
 pygame.display.set_caption("Luca Game")
 
 running = True
+flag_mostrar_texto_segundos = False
+
 
 posicion_circulo = [200, 600]
 posicion_circulo_2 = [100, 600]
@@ -21,8 +23,26 @@ speed_x = 0
 speed_y = 0
 
 # TIMER
-timer_segundo = pygame.USEREVENT
-pygame.time.set_timer(timer_segundo, 10)
+timer = pygame.USEREVENT
+pygame.time.set_timer(timer, 10)
+timer_5_seg = pygame.USEREVENT
+pygame.time.set_timer(timer_5_seg, 5000)
+
+
+# LEER UNA IMAGEN
+imagen_dona = pygame.image.load(r"Nuevo-Python\Practica\Recursos\00.png")
+imagen_dona = pygame.transform.scale(imagen_dona,(50, 50))
+
+background = pygame.image.load(r"Nuevo-Python\Practica\Recursos\background.png")
+background = pygame.transform.scale(background, (1200, 700))
+
+
+# CREAR UN TEXTO
+fuente = pygame.font.SysFont("Arial", 30) # el numero es la altura
+texto = fuente.render("Bienvenido a Luca Game", True, colores.BROWN) # lo transforma en una imagen(al texto), tipo superficie
+
+texto_segundos = pygame.font.SysFont("Arial", 30)
+texto_segundos = texto_segundos.render("Ya pasaron 5 segundos", True, colores.RED)
 
 
 
@@ -33,11 +53,14 @@ while running:
         #if event.type == pygame.MOUSEBUTTONDOWN:
             #print(event.pos) # marca la posicion en donde haces click
         if event.type == pygame.MOUSEBUTTONDOWN:
-            #posicion_circulo = list(event.pos) # donde hago click pone el centro del circulo
-            print(event.pos)
+            posicion_circulo_2 = list(event.pos) # donde hago click pone el centro del circulo
+            #print(event.pos)
         if event.type == pygame.USEREVENT:
-            if event.type == timer_segundo:
-                #print("ya paso un segundo")
+            if event.type == timer_5_seg:
+                flag_mostrar_texto_segundos = True
+
+            if event.type == timer:
+                #print("ya paso un x segundo")
                 # if posicion_circulo[0] > 400:
                 #     posicion_circulo[0] *= -1
                 #     if posicion_circulo[0] < 200:
@@ -48,12 +71,10 @@ while running:
                 else:
                     posicion_circulo[0] = -10
                     # ESTE IF ELSE LO PUEDO  USAR POR EJEMPLO SI QUIERO QUE EL FONDO SE VEA MOVIENDOSE, EJEMPLO UN AUTO EN LA RUTA AVANZANDO
-        
 
 
-            #EVENTOS TECLADO 
+        # EVENTOS TECLADO
         if(event.type == pygame.KEYDOWN):
-
 
             if(event.key == pygame.K_LEFT):
                 speed_x = -1
@@ -63,7 +84,17 @@ while running:
                 speed_y = -1
             if(event.key == pygame.K_DOWN):
                 speed_y = 1
-
+            
+            if(event.key == pygame.K_a):                    # CON ESTA FORMA NO SE PUEDE MANTENER
+                posicion_circulo_2[0] = posicion_circulo_2[0] - 10
+            if(event.key == pygame.K_d):
+                posicion_circulo_2[0] = posicion_circulo_2[0] + 10
+            if(event.key == pygame.K_w):
+                posicion_circulo_2[1] = posicion_circulo_2[1] - 10
+            if(event.key == pygame.K_s):
+                posicion_circulo_2[1] = posicion_circulo_2[1] + 10
+            
+            
         if(event.type == pygame.KEYUP):
 
             if(event.key == pygame.K_LEFT):
@@ -74,23 +105,38 @@ while running:
                 speed_y = 0
             if(event.key == pygame.K_DOWN):
                 speed_y = 0
+
+            if(event.key == pygame.K_a):                    # CON ESTA FORMA NO SE PUEDE MANTENER
+                posicion_circulo_2[0]
+            if(event.key == pygame.K_d):
+                posicion_circulo_2[0]
+            if(event.key == pygame.K_w):
+                posicion_circulo_2[1]
+            if(event.key == pygame.K_s):
+                posicion_circulo_2[1]
             
 
-
+    # lista_teclas = pygame.key.get_pressed()  #        FORMA DEL PROFE PARA QUE SI SE PUEDA MANTENER
+    # if  lista_teclas[pygame.K_a]:                      # en cada vuelta del while lo lee, no es por eventos esta forma
+    #     posicion_circulo_2[0] = posicion_circulo_2[0] - 0.1
 
     screen.fill(colores.WHITE)
 
     coordenada_x += speed_x
     coordenada_y += speed_y
 
-    #---------------ZONA DE DIBUJO------------------
-    pygame.draw.rect(screen, colores.ORANGE, (0, 600, 1200, 700)) # donde empieza x, y, donde terminan x, y
+    #---------------ZONA DE DIBUJO----------------------------------------
+    screen.blit(background, (0, 0))
+    #pygame.draw.rect(screen, colores.ORANGE, (0, 600, 1200, 700)) # donde empieza x, y, donde terminan x, y
     pygame.draw.circle(screen, colores.BROWN, posicion_circulo, 10)
     pygame.draw.circle(screen, colores.PINK, posicion_circulo_2, 30)
-    pygame.draw. rect(screen, colores.BLACK, [coordenada_x, coordenada_y, 50, 50])
+    pygame.draw.rect(screen, colores.BLACK, [coordenada_x, coordenada_y, 50, 50])
 
-    #---------------ZONA DE DIBUJO------------------      
-      
+    screen.blit(imagen_dona, (150, 150)) # posicion
+    screen.blit(texto, (450, 50))
+    if flag_mostrar_texto_segundos: # si esta flag esta en true ...
+        screen.blit(texto_segundos, (10, 20))
+    #---------------ZONA DE DIBUJO----------------------------------------      
 
     pygame.display.flip()
 pygame.quit()
